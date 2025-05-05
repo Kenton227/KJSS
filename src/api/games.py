4f20bfeef5371e5e6d1cdb354bb030b6a83a53d7
Sam Phan <nsphan1207@gmail.com>
2025-05-04 19:11:15 -0700

    feat: set up docs and api endpoint files

---
+++ src/api/games.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from fastapi import APIRouter, Depends, status
+from src.api import auth
+import sqlalchemy
+from src import database as db
+
+router = APIRouter(
+    prefix="/games",
+    tags=["games"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+class Game(BaseModel):
+    black_player_id: int
+    white_player_id: int
+
+@router.post("/{game_id}", response_model=Game)
+def get_game(game_id: int):
+    #TODO: function gets game data given a game id
+    pass
