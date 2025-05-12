2de29b4e1035395eb2f9cc1995b9b80abebabd68
Euclid <eperegri@calpoly.edu>
2025-05-12 10:07:04 -0700

    added showcase comment table and code

--- src/api/showcases.py
+++ src/api/showcases.py
@@ -4,6 +4,8 @@ from src.api import auth
 import sqlalchemy
 from src import database as db
 
+from datetime import datetime
+
 router = APIRouter(
     prefix="/showcases",
     tags=["showcases"],
@@ -21,6 +23,12 @@ class ShowcaseRequest(BaseModel):
 class EditRequest(BaseModel):
     title: str
     caption: str
+
+class comment(BaseModel):
+    post_id: int
+    auther_uid: int
+    date_posted: datetime
+    comment_string: str
 
 
 @router.post("/post", status_code=status.HTTP_204_NO_CONTENT)
@@ -75,4 +83,30 @@ def edit_showcase(showcase_id: int, new_data: EditRequest) -> None:
                     """
                 ),
                 [{"id": showcase_id, "new_caption": new_data.caption}],
+            )
+
+@router.post("/showcases/{showcase_id}/comment", status_code=status.HTTP_204_NO_CONTENT)
+def post_comment(comment_content: comment, showcase_id: int):
+    with db.engine.begin() as connection:
+        #makes sure comment string isn't empty
+        if comment_content.comment_string != "":
+            connection.execute(
+                sqlalchemy.text(
+                    """
+                    INSERT INTO showcase_comments (post_id, author_id, showcase_id, comment)
+                    VALUES (
+                    :post_id,
+                    :author_id,
+                    :showcase_id,
+                    :comment
+                    )
+                    """
+                ),
+                {
+                    "post_id": comment_content.post_id,
+                    "author_id": comment_content.auther_uid,
+                    "showcase_id": showcase_id,
+                    "comment": comment_content.comment_string
+
+                }
             )
