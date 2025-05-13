ffb3e7f2933012718cb4f1c127ee42c57596fd7f
Kenton227 <klrhoden227@gmail.com>
2025-05-12 19:29:34 -0700

    created reports/post endpt and ran ruff

--- alembic/versions/079499fe7aca_adding_reports_table.py
+++ alembic/versions/079499fe7aca_adding_reports_table.py
@@ -5,6 +5,7 @@ Revises: ae68eccc1992
 Create Date: 2025-05-12 14:46:56.232872
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,8 +13,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '079499fe7aca'
-down_revision: Union[str, None] = 'ae68eccc1992'
+revision: str = "079499fe7aca"
+down_revision: Union[str, None] = "ae68eccc1992"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -23,10 +24,17 @@ def upgrade() -> None:
         "reports",
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id", ondelete="CASCADE")),
-        sa.Column("showcase_id", sa.Integer, sa.ForeignKey("showcases.id", ondelete="CASCADE")),
+        sa.Column(
+            "showcase_id", sa.Integer, sa.ForeignKey("showcases.id", ondelete="CASCADE")
+        ),
         sa.Column("report_brief", sa.TEXT, nullable=False),
-        sa.Column("date_reported", sa.TIMESTAMP, nullable=False, server_default=sa.sql.func.now()),
-        sa.Column("report_details", sa.TEXT)
+        sa.Column(
+            "date_reported",
+            sa.TIMESTAMP,
+            nullable=False,
+            server_default=sa.sql.func.now(),
+        ),
+        sa.Column("report_details", sa.TEXT),
     )
 
 
